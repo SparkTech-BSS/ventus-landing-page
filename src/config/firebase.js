0000ea195a2f16e0a2b333244d08aea9247254ec
SparkTech-BSS <suporte@sparktech.co.ao>
2022-11-25 12:17:11 +0100

    update firebase.js file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,35 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDyXOdc7IqwAOXOYqxsTHIiZOD_lNHFGZI',
-  authDomain: 'ventusfirebase.firebaseapp.com',
-  projectId: 'ventusfirebase',
-  storageBucket: 'ventusfirebase.appspot.com',
-  messagingSenderId: '948934660042',
-  appId: '1:948934660042:web:2a95493b38556c8bf10933',
-};
+function initializeAppIfNecessary() { 
+  try {
+    return getApp();
+  } catch (any) {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDyXOdc7IqwAOXOYqxsTHIiZOD_lNHFGZI',
+      authDomain: 'ventusfirebase.firebaseapp.com',
+      projectId: 'ventusfirebase',
+      storageBucket: 'ventusfirebase.appspot.com',
+      messagingSenderId: '948934660042',
+      appId: '1:948934660042:web:2a95493b38556c8bf10933',
+    };
+    return initializeApp(firebaseConfig);
+  }
+}
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDyXOdc7IqwAOXOYqxsTHIiZOD_lNHFGZI',
+//   authDomain: 'ventusfirebase.firebaseapp.com',
+//   projectId: 'ventusfirebase',
+//   storageBucket: 'ventusfirebase.appspot.com',
+//   messagingSenderId: '948934660042',
+//   appId: '1:948934660042:web:2a95493b38556c8bf10933',
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+const app = initializeAppIfNecessary();
 export const storage = getStorage(app);
 
 
