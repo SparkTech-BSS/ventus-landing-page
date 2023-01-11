075d2c8d94f6b6516b5853a9c78ebba11edf236f
caioteca <caiodev2020@gmail.com>
2023-01-11 15:49:11 +0100

    Component Hero Helper Page

--- src/pages/experment/index.tsx
+++ src/pages/experment/index.tsx
@@ -1,13 +1,17 @@
 import { Button } from "components/Experment/Experment1/Button";
 import { CardValor } from "components/Experment/Experment1/CardValor";
+import { Search } from "components/Experment/Experment1/Search";
 import Layout from "components/Layout";
-
+import styles from "./styles.module.scss";
 export default function Experment() {
   return (
     <>
       <Layout>
-        <h1>Welcome To Jomanji!</h1>
-        <CardValor  />
+        <section className={styles.container}>
+        
+          <Search textTitle="Organizadores de eventos"/>
+        </section>
+        
       </Layout>
     </>
   );
